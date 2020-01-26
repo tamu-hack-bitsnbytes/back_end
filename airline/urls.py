5e058c9d102be549accbd71550458fe0509d6216
Harsha <harshavardhanr.993@gmail.com>
2020-01-25 19:37:33 -0600

    Migrated to Django

--- airline/urls.py
+++ airline/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('get_flights/', views.get_flights, name='get_flights'),
-    path('get_donations/', views.get_donations, name='get_donations'),
+    url('', views.index, name='index'),
+    url('get_flights/', views.get_flights, name='get_flights'),
+    url('get_donations/', views.get_donations, name='get_donations'),
 ]
